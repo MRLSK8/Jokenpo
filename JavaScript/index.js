e217aa7684c3093b47cbbde9220e4c92a919b2ca
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-03-06 15:17:34 -0300

    Mostra quando o jogador ou o computador ganha

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,10 @@
 var computadorEscolha = -1;
 var jogadorEscolha = -1;
 var ganhador = -1;
+var pontosParaGanhar = 5;
 
 function jogar(escolha){
-    computadorEscolha = Math.floor(Math.random() * (3-1 + 1) + 1);
+    computadorEscolha = Math.floor(Math.random() * (3-1 + 1) + 1); // Random entre 1 e 3;
     jogadorEscolha = escolha;
 
     ganhador =  EncontraGanhador(jogadorEscolha, computadorEscolha);
@@ -11,22 +12,31 @@ function jogar(escolha){
     document.getElementById("jogador-escolha-" + jogadorEscolha).classList.add("selecionado");
     document.getElementById("computador-escolha-" + computadorEscolha).classList.add("selecionado");
 
-
+    // Verifica quem fez ponto
     if(ganhador == 0){
-        document.getElementById("mensagens").innerHTML = 'Empate';
+        document.getElementById("mensagen").innerHTML = 'Empate';
     }else if(ganhador == 1){
-        document.getElementById("mensagens").innerHTML = 'O jogador ' +  document.getElementById('jogador-nome').innerHTML + ' ganhou';
+        document.getElementById("mensagen").innerHTML = 'O jogador ' +  document.getElementById('jogador-nome').innerHTML + ' ganhou';
         document.getElementById("jogador-pontos").innerHTML = parseInt(document.getElementById("jogador-pontos").innerHTML) + 1;
     }else{
-        document.getElementById("mensagens").innerHTML = 'O computador ganhou';
+        document.getElementById("mensagen").innerHTML = 'O computador ganhou';
         document.getElementById("computador-pontos").innerHTML = parseInt(document.getElementById("computador-pontos").innerHTML) + 1;
     }
 
-    setTimeout(() => {
-        document.getElementById("mensagens").innerHTML = '...';
-        document.getElementById("jogador-escolha-" + jogadorEscolha).classList.remove("selecionado");
-        document.getElementById("computador-escolha-" + computadorEscolha).classList.remove("selecionado");
-    }, 1000);
+    // Verifica se alquem ganhou
+    if(parseInt(document.getElementById("jogador-pontos").innerHTML) == pontosParaGanhar){
+        NovoJogo();
+        document.getElementById("mensagen").innerHTML = ' Parabéns ' + document.getElementById('jogador-nome').innerHTML + ', você ganhou o jogo!';
+    }else if(parseInt(document.getElementById("computador-pontos").innerHTML) == pontosParaGanhar){
+        NovoJogo();
+        document.getElementById("mensagen").innerHTML = ' ahh que pena :(\n O computador ganhou o jogo!\n Click em novo jogo para jogar novamente!';
+    }else{
+        setTimeout(() => {
+            document.getElementById("mensagen").innerHTML = '...';
+            document.getElementById("jogador-escolha-" + jogadorEscolha).classList.remove("selecionado");
+            document.getElementById("computador-escolha-" + computadorEscolha).classList.remove("selecionado");
+        }, 800);
+    }
 
 }
 
@@ -56,4 +66,7 @@ function EncontraGanhador(jogadorEscolha, computadorEscolha){
 function NovoJogo(){
     document.getElementById("jogador-pontos").innerHTML = 0;
     document.getElementById("computador-pontos").innerHTML = 0;
+    document.getElementById("jogador-escolha-" + jogadorEscolha).classList.remove("selecionado");
+    document.getElementById("computador-escolha-" + computadorEscolha).classList.remove("selecionado");
+    document.getElementById("mensagen").innerHTML = '...';
 }
